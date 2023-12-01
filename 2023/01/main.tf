db9d23a55b15e783279db42c1a1b323de4f21b09
Jérôme Pin <jeromepin@users.noreply.github.com>
2023-12-01 15:37:30 +0100

    add 2023-01 second part

--- 2023/01/main.tf
+++ 2023/01/main.tf
@@ -2,19 +2,18 @@ locals {
   # Load all calibration values from the file into an array 
   inputs = split("\n", file("${path.module}/input"))
 
-  # Extract digits from every calibration values
-  calibrations_numbers = [for calibration_value in local.inputs: regexall("[0-9]{1}", calibration_value)]
+  # replace all numbers written as strings with a similar string containg the number : one -> on1e
+  # It allows to easily handle overlapping numbers : nineight (with the common "e") -> nin9eigh8t (-> 98)
+  replaced_strings = compact([for str in local.inputs: replace(replace(replace(replace(replace(replace(replace(replace(replace(str, "nine", "nin9e"), "eight", "eigh8t"), "seven", "seve7n"), "six", "si6x"), "five", "fiv5e"), "four", "fou4r"), "three", "thre3e"), "two", "tw2o"), "one", "on1e")])
+
+  # Extract digits from every strings
+  extracted_digits = [for str in local.replaced_strings: regexall("[0-9]{1}", str)]
   
-  # Every single digit is returned twice into a list : [7] => "77"
-  single_digits = [for sublist in local.calibrations_numbers: "${sublist[0]}${sublist[0]}" if length(sublist) == 1]
-  
-  # When a calibration value has multiple digits, the first and last one are returned : [1, 2, 3, 4, 5] => "15"
-  several_digits = [for sublist in local.calibrations_numbers: "${sublist[0]}${reverse(sublist)[0]}" if length(sublist) > 1]
-  
-  res = sum(concat(local.single_digits, local.several_digits))
+  # the first and last digit are returned : [1, 2, 3, 4, 5] => "15"
+  calibration_value = [for sublist in local.extracted_digits: "${sublist[0]}${reverse(sublist)[0]}"]
 }
 
 output "out" {
-  value = local.res
+  value = sum(local.calibration_value)
   sensitive = false
 }
